a19bf538fa386639d1b0ca45d8342ea75298492d
MichielN19 <Michielmitsjol@gmail.com>
2024-10-07 17:27:02 +0200

    fixed the nav-bar

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = 3000;
+const __dirname = path.resolve();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public"))); 
 
 
 app.get("/", (req, res) => {
-   res.sendFile("/index.html");
+   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/wat-ik-doe", (req, res) => {
-   res.send("Dit is de wat die ik pagina");
+   res.sendFile(path.join(__dirname, "public", "views", "wat-doe-ik.html"));
 });
 
 app.get("/contact", (req, res) => {
-   res.send("Dit is de contact pagina");
+   res.sendFile(path.join(__dirname, "public", "views", "contact.html")); 
 });
 
 app.listen(port, () => {
    console.log(`Server is running on port ${3000}`);
-})
+});
---
+++ public/index.js
@@ -0,0 +1,10 @@
+
+function OpenSidebar() {
+   const sidebar = document.querySelector(".sidebar");
+   sidebar.style.display = 'flex';
+}
+
+function CloseSidebar() {
+   const sidebar = document.querySelector(".sidebar");
+   sidebar.style.display = 'none';
+}
