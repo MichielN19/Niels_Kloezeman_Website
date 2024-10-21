a8fa983480e974587d2848365d35e360d4ad0401
MichielN19 <Michielmitsjol@gmail.com>
2024-10-21 14:02:04 +0200

    new contact page and fixed some things

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "public"))); 
